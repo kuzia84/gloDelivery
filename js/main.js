4c75052b08d6b0b9785081d177679ce5a4576ed1
Vasiliy Chumachenco <kuzia84@gmail.com>
2020-10-30 13:50:32 +0200

    day2

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
-const cartButton = document.querySelector("#cart-button");
-const modal = document.querySelector(".modal");
-const close = document.querySelector(".close");
+"use ctrict";
 
-cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
-
-function toggleModal() {
-  modal.classList.toggle("is-open");
-}
-
-const buttonAuth = document.querySelector(".button-auth"),
+const cartButton = document.querySelector("#cart-button"),
+  modal = document.querySelector(".modal"),
+  close = document.querySelector(".close"),
+  buttonAuth = document.querySelector(".button-auth"),
   buttonCart = document.querySelector("#button-cart"),
   buttonOut = document.querySelector(".button-out"),
   userName = document.querySelector(".user-name"),
   modalAauth = document.querySelector(".modal-auth"),
   closeAuth = document.querySelector(".close-auth"),
   logInForm = document.querySelector("#logInForm"),
-  loginInput = document.querySelector("#login");
+  loginInput = document.querySelector("#login"),
+  cardsRestaurants = document.querySelector(".cards-restaurants"),
+  containerPromo = document.querySelector(".container-promo"),
+  restaurants = document.querySelector(".restaurants"),
+  menu = document.querySelector(".menu"),
+  logo = document.querySelector(".logo"),
+  cardsMenu = document.querySelector(".cards-menu");
 
 let login = localStorage.getItem("gloDelivery");
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
 
 function toggleModalAuth() {
   modalAauth.classList.toggle("is-open");
@@ -92,4 +96,85 @@ function checkAuth() {
   }
 }
 
+function createCardsRestaurants() {
+  const card = `
+  <a class="card card-restaurant">
+    <img src="img/pizza-plus/preview.jpg" alt="image" class="card-image" />
+    <div class="card-text">
+      <div class="card-heading">
+        <h3 class="card-title">Пицца плюс</h3>
+        <span class="card-tag tag">50 мин</span>
+      </div>
+      <div class="card-info">
+        <div class="rating">
+          4.5
+        </div>
+        <div class="price">От 900 ₽</div>
+        <div class="category">Пицца</div>
+      </div>
+    </div>
+  </a>
+  `;
+  cardsRestaurants.insertAdjacentHTML("beforeend", card);
+}
+
+function creteCardGood() {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.insertAdjacentHTML(
+    "beforeend",
+    `
+      <img src="img/pizza-plus/pizza-vesuvius.jpg" alt="image" class="card-image" />
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title card-title-reg">Пицца Везувий</h3>
+        </div>
+        <div class="card-info">
+          <div class="ingredients">Соус томатный, сыр «Моцарелла», ветчина, пепперони, перец
+            «Халапенье», соус «Тобаско», томаты.
+          </div>
+        </div>
+        <div class="card-buttons">
+          <button class="button button-primary button-add-cart">
+            <span class="button-card-text">В корзину</span>
+            <span class="button-cart-svg"></span>
+          </button>
+          <strong class="card-price-bold">545 ₽</strong>
+        </div>
+      </div>
+    `
+  );
+  cardsMenu.insertAdjacentElement("beforeend", card);
+}
+
+function openGoods(event) {
+  const target = event.target,
+    restaurant = target.closest(".card-restaurant");
+  if (restaurant) {
+    if (login) {
+      containerPromo.classList.add("hide");
+      restaurants.classList.add("hide");
+      menu.classList.remove("hide");
+      cardsMenu.textContent = "";
+      creteCardGood();
+      creteCardGood();
+      creteCardGood();
+    } else {
+      toggleModalAuth();
+    }
+  }
+}
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+cardsRestaurants.addEventListener("click", openGoods);
+logo.addEventListener("click", function () {
+  containerPromo.classList.remove("hide");
+  restaurants.classList.remove("hide");
+  menu.classList.add("hide");
+});
+
 checkAuth();
+createCardsRestaurants();
+createCardsRestaurants();
+createCardsRestaurants();
